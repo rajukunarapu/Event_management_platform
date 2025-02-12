5ad49f6a2b61059d1ddba65ab38f791d6e8a2f2b
rajukunarapu <rajukunarapu49@gmail.com>
2025-02-12 20:55:11 +0530

    vercel deployement commands removed

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,4 +6,4 @@ connectDB();
 
 modules.exports = app
 
-// app.listen(process.env.PORT, ()=>console.log(`Server running on ${process.env.PORT} port`))
+app.listen(process.env.PORT, ()=>console.log(`Server running on ${process.env.PORT} port`))

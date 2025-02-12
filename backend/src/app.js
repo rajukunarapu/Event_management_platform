e0236933e2b510cacfc56fd4533bbfbcd5df7a73
rajukunarapu <rajukunarapu49@gmail.com>
2025-02-12 20:40:32 +0530

    added cors policy

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,12 @@ const app = express();
 
 // app.use(cookieparser())
 app.use(express.json());
-app.use(cors("*"));
+
+app.use(cors({
+    origin: `${process.env.FRONTEND_URL}`,
+    credentials: true,
+}));
+
 app.use('/auth', authRoutes);
 app.use('/user',userRoutes);
 
